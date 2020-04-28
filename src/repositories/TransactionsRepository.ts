49e5c634c30ea6d4768581740338b8ba919f7cdd
Hugo Sukeyosi <hugosukeyosi@gmail.com>
2020-04-28 16:25:45 -0300

    Quarto desafio concluido

---
+++ src/repositories/TransactionsRepository.ts
@@ -0,0 +1,55 @@
+import Transaction from '../models/Transaction';
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface CreateAppointent {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class TransactionsRepository {
+  private transactions: Transaction[];
+
+  constructor() {
+    this.transactions = [];
+  }
+
+  public all(): Transaction[] {
+    return this.transactions;
+  }
+
+  public getBalance(): Balance {
+    const outcome = this.transactions.reduce((acumulator: number, current) => {
+      if (current.type === 'outcome') return acumulator + current.value;
+
+      return acumulator;
+    }, 0);
+    const income = this.transactions.reduce((acumulator: number, current) => {
+      if (current.type === 'income') return acumulator + current.value;
+
+      return acumulator;
+    }, 0);
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
+  }
+
+  public create({ title, value, type }: CreateAppointent): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
+  }
+}
+
+export default TransactionsRepository;
---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,36 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, value, type }: Request): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && balance.total < value) {
+      throw Error('Transaction invalid! Outcome > total');
+    }
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('Type invalid!');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
